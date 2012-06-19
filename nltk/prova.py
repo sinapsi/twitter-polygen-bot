500f6d96f3cacd9b1308ef44f71cbae08e0ad57e
Michele Rappazzo <micheleee@gmail.com>
2012-06-19 18:42:48 +0200

    modified:   nltk/prova.py

--- nltk/prova.py
+++ nltk/prova.py
@@ -1,13 +1,24 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+NLTK :D
+"""
 import nltk
 import sys
 import urllib
 
-rawText = ""
-print "scaricato :O"
+#have you passed all the parameters?
+if (len(sys.argv) < 3):
+     print "execution: " + sys.argv[0] + " number_of_words filename1 filename2 ..."
+     print "example: " + sys.argv[0] + " 100 txt/*"
+     quit()
+     
+rawText = "" #string with all the text from all the files passed
 for currentFile in sys.argv[2:]:
-    in_file = open(currentFile,"r")
+    in_file = open(currentFile, "r")
     rawText += in_file.read()
     in_file.close()
 myText = nltk.Text(nltk.word_tokenize(rawText))
 
-print myText.generate(int(sys.argv[1])) 
+#let's jam
+print myText.generate(int(sys.argv[1]))
